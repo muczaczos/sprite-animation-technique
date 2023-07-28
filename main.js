a261c3ec0d11edcb36f9fee2e842526307f50f4c
Arkadiusz Szatkowski <muczaczos@gmail.com>
2023-07-28 15:21:19 +0200

    Fit sprite from spreedsheet to my canvas

--- main.js
+++ main.js
@@ -4,13 +4,25 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
-playerImage.src = "player.png";
-let x = 0;
+playerImage.src = "shadow_dog.png";
+
+//spreedsheet width is 6786 and has 12 columns. 6876 / 12 = 573
+//I use 575 because the last frame is a bit smaller 
+const spriteWidth = 575; 
+
+//spreedsheet height is 5230 and has 10 columns. 5230 / 10 = 523
+const spriteHeight = 523;
+
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.fillRect(x,50,100,100);
-  x++;
+  //ctx.fillRect(100,50,100,100);
+  /*
+  ctx.drawImage(image, source-x, source-y, 
+  source-width, source-height, destonation-x, 
+  destonation-y, destonation-width, destonation-height)
+  */
+  ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   requestAnimationFrame(animate);
 };
 
